65e971e24bbfd3057ceadfa2ef7dc77cc47f971f
Iohann <iohann.tachy@gmail.com>
2021-01-13 14:35:00 +0000

    Adicionado comentário com " para que o markdown exbia corretamente

--- rm_autorunner.bat
+++ rm_autorunner.bat
@@ -17,6 +17,7 @@ set _Args=%*
 if "%~1" NEQ "" (
   set _Args=%_Args:"=%
 )
+::"
 fltmc 1>nul 2>nul || (
   cd /d "%~dp0"
   cmd /u /c echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~dp0"" && ""%~dpnx0"" ""%_Args%""", "", "runas", 1 > "%temp%\GetAdmin.vbs"
